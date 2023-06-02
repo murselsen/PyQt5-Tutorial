8b410b1258da6295cffd22e17f6a4d58e2b4837a
Mursel SEN <murselsen_35@hotmail.com>
2023-06-02 22:44:22 +0300

    Uygulama Oluşturma Uygulaması

---
+++ 01-CreateApp.py
@@ -0,0 +1,27 @@
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+def window():
+    # sys.argv => gönderilen komutlar
+    
+    # app
+    # Application() -> uygulama oluşturur..
+    app = QApplication(sys.argv) 
+    
+    
+    # uygulamanın Ana Ekranı 
+    # QMainWindow -> Ana Ekran
+    win = QMainWindow()
+    
+    win.setGeometry(80,60,500,500)
+    win.setWindowTitle("First Application")
+    
+    # Show() -> pencereyi gösteriri
+    win.show()
+    # system tarafından EXIT komutu gönderildiğinde 
+    # app işler
+    sys.exit(app.exec_())
+    
+    
+window()
